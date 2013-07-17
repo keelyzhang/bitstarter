aad8c7b7831d2ca20f9d696143332ba752be33f8
Keely Zhang <k.zhang127@gmail.com>
2013-07-17 20:40:59 +0000

    up

--- web.js
+++ web.js
@@ -4,8 +4,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  fs.readFile('index.html', function(err,data){
+	res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+	res.write(data);
+	res.end();
+});
 });
 
 var port = process.env.PORT || 5000;
